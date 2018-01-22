22c1b86715f6728e1917c8ced33229f1bf611088
Faiza Husain and Miranda Keith <faiza@>
2018-01-22 09:17:19 -0800

    Adds JS back-end setup by creating a constructor

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// CONSTRUCTOR
+function Place (name, location, landmarks){
+  this.name = name;
+  this.location = location;
+  this.landmarks = [];
+  this.notes = [];
+}
+
+var losAngeles = new Place("Los Angeles", "California");
